b0c31ec1208cf9e964b659fc60b7ed03261b105a
Michel Nowak <michel.nowak@prevision.io>
2020-10-20 11:33:01 +0200

    fix(install): add requirements install to setup.py in order to directly install all dependencies from pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 from setuptools import setup, find_packages
 
-
+install_requirements = [
+    "setuptools",
+    "numpy",
+    "tensorflow",
+    "sympy",
+    "matplotlib",
+    "strawberryfields>=0.15",
+    "pennylane",
+    "pennylane-sf",
+    "lightgbm",
+    "openml",
+    "pandas",
+    "scikit-learn",
+    "tqdm",
+    "gym",
+    "box2d-py",
+    "imageio"
+]
 setup(name='prevision-quantum-nn',
       version='1.0',
       description='Prevision Automating Quantum Neural Networks Applications',
@@ -8,6 +25,7 @@ setup(name='prevision-quantum-nn',
       author_email='prevision@prevision.io',
       license='MIT',
       packages=find_packages(),
+      install_requires=install_requirements,
       zip_safe=False,
       python_requires = ">=3.6.8"
 )
