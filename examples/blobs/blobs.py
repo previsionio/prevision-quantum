dc018404cef7f83e7ca502166ea5d61aa2034a5c
aymeric-degroote <aymeric.degroote@gmail.com>
2021-12-09 16:12:44 +0100

    feat: add examples

---
+++ examples/blobs/blobs.py
@@ -0,0 +1,69 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+import prevision_quantum_nn as qnn
+import numpy as np
+
+if __name__ == "__main__":
+    # prepare data
+    centers = [[np.pi * 0.5, np.pi * 0.5], [np.pi * 1.5, np.pi * 1.5]]
+    cluster_std = np.pi * 0.2
+
+    num_samples = 500
+    X, y = datasets.make_blobs(n_samples=num_samples, random_state=0,
+                               centers=centers, cluster_std=cluster_std)
+
+    # shift label from {0, 1} to {-1, 1}
+    y = y * 2 - np.ones(len(y))
+    x_train, x_val, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=40)
+
+    # build dataset
+    dataset = qnn.get_dataset_from_numpy(x_train,
+                                         y_train,
+                                         val_features=x_val,
+                                         val_labels=y_test)
+
+    # customize preprocessing
+    preprocessing_params = {
+        "polynomial_degree": 2
+    }
+
+    # customize model
+    model_params = {
+        "architecture": "qubit",
+        "num_q": 5,
+        "encoding": "angle",
+        "use_early_stopper": True,
+        "early_stopper_patience": 20,
+        "max_iterations": 10000,
+        "interface": "autograd",
+        "layer_type": "template",
+        "snapshot_frequency": 5,
+        "verbose": True,
+        "prefix": "blobs",
+        "num_layers": 4,
+        "optimizer_name": "Adam",
+        "learning_rate": 0.05,
+    }
+
+    # customize postprocessing
+    postprocessing_params = {
+        "phase_space_plotter": {
+            "dim": 2,
+            "min_max_array": [[min(X[:, 0]), max(X[:, 0])],
+                              [min(X[:, 1]), max(X[:, 1])]],
+            "prefix": "blobs"
+        }
+    }
+
+    # build application
+    application = qnn.get_application(
+        "classification",
+        prefix="blobs",
+        preprocessing_params=preprocessing_params,
+        model_params=model_params,
+        postprocessing_params=postprocessing_params)
+
+    # solve application
+    application.solve(dataset)
---
+++ examples/metrics/correlation_expr_accuracy.py
@@ -0,0 +1,108 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+import prevision_quantum_nn as qnn
+
+if __name__ == "__main__":
+
+    # prepare data
+    centers = [[np.pi * 0.5, np.pi * 0.5], [np.pi * 1.5, np.pi * 1.5]]
+    cluster_std = np.pi * 0.2
+
+    X, y = datasets.make_blobs(n_samples=500, random_state=0, centers=centers,
+                               cluster_std=cluster_std)
+
+    # shift label from {0, 1} to {-1, 1}
+    y = y * 2 - np.ones(len(y))
+
+    x_temp, x_test, y_temp, y_test = train_test_split(X, y, test_size=0.25,
+                                                      random_state=42)
+    x_train, x_val, y_train, y_val = train_test_split(x_temp, y_temp,
+                                                      test_size=0.25,
+                                                      random_state=42)
+
+    dataset = qnn.get_dataset_from_numpy(x_train, y_train,
+                                         val_features=x_val, val_labels=y_val)
+
+    layer_names = ["basic_circuit_1",
+                   "basic_circuit_2",
+                   "basic_circuit_3",
+                   "basic_circuit_4",
+                   "basic_circuit_5",
+                   "basic_circuit_6"]
+
+    accs = []
+    exprs = []
+    ents = []
+    for layer_name in layer_names:
+
+        print(layer_name)
+        prefix = "corr_expr_acc" + layer_name[-1]
+        # customize model
+        model_params = {
+            "layer_name": layer_name,
+            # "num_q": 2,
+            "num_layers": 2,
+            # "use_early_stopper": True,
+            # "early_stopper_patience": 20,
+            "early_stopper_epsilon": 0.3,
+            "max_iterations": 101,
+            "prefix": prefix,
+        }
+
+        # customize postprocessing
+        postprocessing_params = {
+            "phase_space_plotter": {
+                "dim": 2,
+                "min_max_array": [[0, 2*np.pi],
+                                  [0, 2*np.pi]],
+                "prefix": prefix,
+            }
+        }
+
+        classification_application = qnn.get_application(
+            "classification",
+            prefix=prefix,
+            model_params=model_params,
+            postprocessing_params=postprocessing_params
+        )
+        classification_application.solve(dataset)
+
+        y_pred = classification_application.predict(x_test)
+        y_pred = 0.5 + 0.5 * y_pred
+
+        acc = 0
+        for l_true, l_pred in zip(y_test, y_pred):
+            if abs(l_true - l_pred) < 1e-5:
+                acc = acc + 1
+        accuracy = acc / len(y_test)
+        print("accuracy", accuracy)
+
+        # build application
+        descriptor_application = qnn.get_application(
+            "descriptor_computation",
+            prefix="desc",
+            model_params=model_params
+        )
+
+        expr = descriptor_application.compute(dataset, "expressibility")
+        ent = descriptor_application.compute(dataset, "entangling_capability")
+
+        print("expr", expr)
+        print("ent", ent)
+
+        accs.append(acc)
+        exprs.append(expr)
+        ents.append(ent)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(accs, exprs)
+    plt.xlabel('accuracy')
+    plt.ylabel('expressibility')
+    plt.show()
+
+    plt.plot(accs, ents)
+    plt.xlabel('accuracy')
+    plt.ylabel('entanglement')
+    plt.show()
