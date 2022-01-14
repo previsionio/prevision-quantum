5767208d765d09420c62b300b5ae2f66f9906d18
aymeric-degroote <aymeric.degroote@gmail.com>
2022-01-14 18:11:42 +0100

    update(metrics example): state vs shots

--- examples/metrics/state_vs_shots.py
+++ examples/metrics/state_vs_shots.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     circuit_bool = True
     num_q = 1
-    n_sample = 2
+    seed = 12
 
     for shots in [10, 100, 1000]:
         print("\nnb shots =", shots)
@@ -18,16 +18,16 @@ if __name__ == "__main__":
             qml.StronglyEntanglingLayers(variables, wires=range(num_q))
             return qml.expval(qml.PauliZ(0))
 
-        np.random.seed(12)
-        for _ in range(n_sample):
+        np.random.seed(seed)
+        for _ in range(2):
             var = np.random.random((1, num_q, 3))
             print("var", var)
 
             if circuit_bool:
                 circuit(var)
 
-        npr = np.random.RandomState(12)
-        for _ in range(n_sample):
+        npr = np.random.RandomState(seed)
+        for _ in range(2):
             var_npr = npr.random((1, num_q, 3))
             print("var_npr", var_npr)
 
